40f6ba6cae9ee3d2cde9c0f87c0954f911a78227
jeeva2334 <jeeva.v910@gmail.com>
2023-12-05 13:25:51 +0530

    added two projects

---
+++ Blockchain-E-Learning/src/Components/trusted/Trusted.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./trusted.css"
+export default function Trusted() {
+  return (
+    <div className='trusted'>
+        <h1 className='trust-heading'>Trusted by Leading Product Desingner by</h1>
+        <ul className='agent'>
+            <li className='agent-company'><a href="https://about.google/" title="About Google"><img src="https://www.freepnglogos.com/uploads/google-logo-png/file-google-logo-svg-wikimedia-commons-23.png" width="200" alt="file google logo svg wikimedia commons" /></a></li>
+            <li className='agent-company'><a href="https://www.ibm.com/in-en/about" title="About IBM"><img src="https://www.freepnglogos.com/uploads/ibm-logo-png/ibm-logo-png-transparent-svg-vector-bie-supply-3.png" width="200" alt="ibm logo png transparent svg vector bie supply" /></a></li>
+            <li className='agent-company'><a href="https://www.aboutamazon.com/" title="About Amazon"><img src="https://www.freepnglogos.com/uploads/amazon-png-logo-vector/woodland-gardening-amazon-png-logo-vector-8.png" width="200" alt="woodland gardening amazon png logo vector" /></a></li>
+            <li className='agent-company'><a href="https://www.apple.com/in/business/" title="About Apple"><img src="https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-dallas-shootings-don-add-are-speech-zones-used-4.png" width="200" alt="apple logo png dallas shootings don add are speech zones used" /></a></li>
+            <li className='agent-company'><a href="https://www.microsoft.com/en-in/about/" title="About Microsoft"><img src="https://www.freepnglogos.com/uploads/microsoft-logo-png-transparent-background-1.png" width="200" alt="microsoft logo png transparent background" /></a></li>
+            <li className='agent-company'><a href="https://about.meta.com/" title="About Meta"><img src="https://www.freepnglogos.com/uploads/logo-facebook-png/logo-facebook-facebook-logo-png-transparent-svg-vector-bie-supply-13.png" width="200" alt="Facebook Icon Original Logo 2020" /></a></li>
+            <li className='agent-company'><a href="https://www.intc.com/about-intel" title="About Intel"><img src="https://www.freepnglogos.com/uploads/intel-png-logo/intel-is-shipping-its-kaby-lake-processors-png-logo-1.png" width="200" alt="intel is shipping its kaby lake processors png logo" /></a></li>
+            <li className='agent-company'><a href="https://www.tesla.com/about" title="About Tesla"><img src="https://www.freepnglogos.com/uploads/tesla-logo-png-20.png" width="200" alt="tesla logo png" /></a></li>
+            <li className="agent-company"><a href="https://www.flipkartcareers.com/#!/aboutus" title="About Flipkart"><img src="https://www.freepnglogos.com/uploads/flipkart-logo-png/flipkart-logo-transparent-vector-3.png" width="200" alt="flipkart logo transparent vector" /></a></li>
+        </ul>
+    <div class="hero bg-dark text-secondary px-4 py-5 text-center">
+    <div class="py-5">
+      <h1 class="display-5 fw-bold text-white">Who Are We ?</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="fs-5 mb-4">Welcome to De-Learn ! We are at the forefront of education's evolution, embracing the future with the power of blockchain. Our decentralized platform offers unparalleled security, transparency, and accessibility, revolutionizing the way you learn. With blockchain technology, your achievements and credentials are securely stored, and smart contracts streamline processes for seamless interactions. Gain ownership of your accomplishments through verifiable certificates and connect with learners worldwide in our thriving community. Expert instructors ensure an immersive and interactive learning experience, empowering you to unlock your true potential. Embrace the future of education with us and embark on a journey that knows no boundaries. Join De-Lear now and transform the way you learn!</p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold"></button>
+          <button type="button" class="btn btn-outline-light btn-lg px-4"></button>
+        </div>
+      </div>
+    </div>
+  </div>
+    </div>
+  )
+}
